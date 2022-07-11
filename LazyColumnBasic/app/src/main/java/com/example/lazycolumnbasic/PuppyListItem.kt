36ae6550febf2d83fa9cf3b5c57d7ee80a539bd9
이승용 <dltmddyd321@naver.com>
2022-07-11 15:44:58 +0900

    Apply-Coil-ImageLibrary

--- LazyColumnBasic/app/src/main/java/com/example/lazycolumnbasic/PuppyListItem.kt
+++ LazyColumnBasic/app/src/main/java/com/example/lazycolumnbasic/PuppyListItem.kt
@@ -12,10 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.example.lazycolumnbasic.data.Puppy
 
 @Composable
@@ -30,9 +32,9 @@ fun PuppyListItem(puppy: Puppy, navigateToProfile: (Puppy) -> Unit) {
         Row(
             Modifier.clickable { navigateToProfile(puppy) }
         ) {
-            PuppyImage(puppy = puppy)
+            PuppyImageWithCoil(puppy = puppy)
             Column (
-                modifier = androidx.compose.ui.Modifier
+                modifier = Modifier
                     .padding(16.dp)
                     .fillMaxWidth()
                     .align(Alignment.CenterVertically)
@@ -45,9 +47,23 @@ fun PuppyListItem(puppy: Puppy, navigateToProfile: (Puppy) -> Unit) {
 }
 
 @Composable
-private fun PuppyImage(puppy: Puppy) {
+private fun PuppyImageWithCoil(puppy: Puppy) {
+    AsyncImage(model = ImageRequest.Builder(LocalContext.current)
+        .data(puppy.puppyImageId)
+        .crossfade(true)
+        .build(),
+    contentDescription = null,
+    contentScale = ContentScale.Crop,
+    modifier = Modifier
+        .padding(8.dp)
+        .size(84.dp)
+        .clip(RoundedCornerShape(corner = CornerSize(16.dp))))
+}
+
+@Composable
+private fun PuppyImageWithBasic(puppy: Puppy) {
     Image(
-        painter = painterResource(id = puppy.puppyImageId), 
+        painter = painterResource(id = puppy.puppyImageId),
         contentDescription = null,
         contentScale = ContentScale.Crop,
         modifier = Modifier
