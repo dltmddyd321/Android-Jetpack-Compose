6a90b64ed91cf3153d4e90d60760ce9e042fc191
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2022-07-30 22:06:16 -0700

    ImageButton
    - Spacer
    - painterResource

--- BookingDesign/app/src/main/java/com/example/bookingdesign/MainActivity.kt
+++ BookingDesign/app/src/main/java/com/example/bookingdesign/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.bookingdesign
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
@@ -27,7 +30,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            HomeComponent()
         }
     }
 }
@@ -40,7 +43,119 @@ fun HomeComponent() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        TopSection()
+        Column {
+            TopSection()
+            Spacer(modifier = Modifier.size(20.dp))
+            Event()
+        }
+    }
+}
+
+@Composable
+fun Event() {
+    val textColor = Color(0xFF222222)
+
+    return Row(Modifier.fillMaxWidth()) {
+        Column(
+            modifier = Modifier.weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(68.dp)
+                    .clip(RoundedCornerShape(18.dp))
+                    .background(Color(0xFFF1F6F7)),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_baseline_house_24),
+                    contentDescription = "Home"
+                )
+            }
+            Spacer(modifier = Modifier.size(6.dp))
+            Text(
+                text = "HOME",
+                color = textColor,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp,
+                letterSpacing = 0.2.sp
+            )
+        }
+        Column(
+            modifier = Modifier.weight(1F),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(68.dp)
+                    .clip(shape = RoundedCornerShape(18.dp))
+                    .background(Color(0xFFFFF5F5)),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_fly),
+                    contentDescription = "Flights"
+                )
+            }
+            Spacer(modifier = Modifier.size(6.dp))
+            Text(
+                text = "HOME",
+                color = textColor,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp,
+                letterSpacing = 0.2.sp
+            )
+        }
+        Column(
+            modifier = Modifier.weight(1F),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(68.dp)
+                    .clip(shape = RoundedCornerShape(18.dp))
+                    .background(Color(0xFFFFF7F2)),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_baseline_fastfood_24),
+                    contentDescription = "Foods"
+                )
+            }
+            Spacer(modifier = Modifier.size(6.dp))
+            Text(
+                text = "Foods",
+                color = textColor,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp,
+                letterSpacing = 0.2.sp
+            )
+        }
+        Column(
+            modifier = Modifier.weight(1F),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(68.dp)
+                    .clip(shape = RoundedCornerShape(18.dp))
+                    .background(Color(0xFFF4F3FB)),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_baseline_emoji_events_24),
+                    contentDescription = "Events"
+                )
+            }
+            Spacer(modifier = Modifier.size(6.dp))
+            Text(
+                text = "EVENTS",
+                color = textColor,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp,
+                letterSpacing = 0.2.sp
+            )
+        }
     }
 }
 
