4dcf14ccb43195008f5aaa7d4870678a442c9f2c
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2022-08-02 22:44:56 -0700

    Kakao-Login-Logout
    - suspendCoroutine
    - ViewModel

--- KakaoAuth/app/src/main/java/com/example/kakaoauth/MainActivity.kt
+++ KakaoAuth/app/src/main/java/com/example/kakaoauth/MainActivity.kt
@@ -3,20 +3,61 @@ package com.example.kakaoauth
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kakaoauth.ui.theme.KakaoAuthTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val kakaoAuthViewModel: KakaoAuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            KakaoAuthTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    KakaoLoginView(kakaoAuthViewModel)
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun KakaoLoginView(viewModel: KakaoAuthViewModel) {
+
+    //State Flow를 구독하여 State를 추출
+    val isLoggedIn = viewModel.isLoggedIn.collectAsState()
+
+    val loginStatusInfoTitle = if(isLoggedIn.value) "로그인 상태" else "로그아웃 상태"
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = { viewModel.kakaoLogin() }) {
+            Text(text = "로그인")
+        }
+        Button(onClick = { viewModel.kakaoLogout() }) {
+            Text(text = "로그아웃")
+        }
+
+        Text(text = loginStatusInfoTitle, textAlign = TextAlign.Center, fontSize = 20.sp)
     }
 }
