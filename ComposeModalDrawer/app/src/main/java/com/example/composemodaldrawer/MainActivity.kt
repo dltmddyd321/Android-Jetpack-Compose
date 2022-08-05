629f1f46c942c66573c9f5683f1de1462403e3be
dltmddyd321 <dltmddyd321@naver.com>
2022-08-05 21:57:59 +0900

    DatePicker-ComposeCalendar

--- ComposeModalDrawer/app/src/main/java/com/example/composemodaldrawer/MainActivity.kt
+++ ComposeModalDrawer/app/src/main/java/com/example/composemodaldrawer/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.composemodaldrawer
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.content.Context
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composemodaldrawer.ui.theme.ComposeModalDrawerTheme
 import kotlinx.coroutines.launch
+import org.intellij.lang.annotations.JdkConstants
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +33,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ModalDrawerSample()
+//                    ModalDrawerSample()
+                    ShowDatePicker(this)
                 }
             }
         }
@@ -68,6 +79,47 @@ fun ModalDrawerSample() {
             }
         }
     )
+}
+
+@SuppressLint("NewApi")
+@Composable
+fun ShowDatePicker(context: Context) {
+
+    val year: Int
+    val month: Int
+    val day: Int
+
+    val calendar = Calendar.getInstance()
+    year = calendar.get(Calendar.YEAR)
+    month = calendar.get(Calendar.MONTH)
+    day = calendar.get(Calendar.DAY_OF_MONTH)
+    calendar.time = Date()
+
+    val date = remember {
+        mutableStateOf("")
+    }
+
+    val datePickerDialog = DatePickerDialog(
+        context,
+        { _: DatePicker, year: Int, month: Int, datOfMonth: Int ->
+            date.value = "$datOfMonth/$month/$year"
+
+        }, year, month, day
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(text = "Selected Date: ${date.value}")
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = {
+            datePickerDialog.show()
+        }) {
+            Text(text = "Open Date Picker!")
+        }
+    }
 }
 
 @Composable
