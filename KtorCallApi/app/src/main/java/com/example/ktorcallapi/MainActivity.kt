0916724eeb945c53f60a9df7d4138e25d377ed35
dltmddyd321 <dltmddyd321@naver.com>
2022-08-08 21:27:16 +0900

    Compose-Glide
    - Download Bitmap
    - Set Image

--- KtorCallApi/app/src/main/java/com/example/ktorcallapi/MainActivity.kt
+++ KtorCallApi/app/src/main/java/com/example/ktorcallapi/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.example.ktorcallapi
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.ktorcallapi.ui.theme.KtorCallApiTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,6 +49,42 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun Profile(imgUrl: String, modifier: Modifier = Modifier) {
+    val bitmap: MutableState<Bitmap?> = mutableStateOf(null)
+
+    val imageModifier = modifier
+        .size(50.dp, 50.dp)
+        .clip(CircleShape)
+
+    //이미지 다운로드
+    Glide.with(LocalContext.current)
+        .asBitmap()
+        .load(imgUrl)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                bitmap.value = resource
+            }
+
+            override fun onLoadCleared(placeholder: Drawable?) {}
+        })
+
+    bitmap.value?.asImageBitmap()?.let { fetchedBitmap ->
+        Image(
+            bitmap = fetchedBitmap,
+            contentScale = ContentScale.Fit,
+            contentDescription = null,
+            modifier = imageModifier
+        )
+    } ?: Image(
+        painter = painterResource(id = R.drawable.ic_launcher_background),
+        contentScale = ContentScale.Fit,
+        contentDescription = null,
+        modifier = imageModifier
+    )
+}
+
 @Composable
 fun UserView(data: User) {
     val typography = MaterialTheme.typography
@@ -48,7 +100,10 @@ fun UserView(data: User) {
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-
+            Profile(imgUrl = data.avatar)
+            Column {
+                Text(text = data.name, style = typography.body1)
+            }
         }
     }
 }
