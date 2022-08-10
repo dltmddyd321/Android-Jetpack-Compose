d03b9f5150e453c5f41d1c9e26923b23772f5165
dltmddyd321 <dltmddyd321@naver.com>
2022-08-10 21:29:30 +0900

    Compose-Internet-Check
    - ConnectivityManager
    - Coroutine Flow
    - Network Callback

---
+++ CheckInternet/app/src/main/java/com/example/checkinternet/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.checkinternet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.checkinternet.ui.theme.CheckInternetTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var connectivityObserver: ConnectivityObserver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        connectivityObserver = NetworkConnectivityObserver(applicationContext)
+        setContent {
+            CheckInternetTheme {
+                val status by connectivityObserver.observe().collectAsState(
+                    initial = ConnectivityObserver.Status.Unavailable
+                )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Network status: $status")
+                }
+            }
+        }
+    }
+}
