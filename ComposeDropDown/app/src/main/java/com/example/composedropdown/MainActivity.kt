5ace724159a2194d2c85b6f621f36be711533bc0
dltmddyd321 <dltmddyd321@naver.com>
2022-09-24 15:54:21 +0900

    DropDown-Box

--- ComposeDropDown/app/src/main/java/com/example/composedropdown/MainActivity.kt
+++ ComposeDropDown/app/src/main/java/com/example/composedropdown/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
@@ -16,9 +17,13 @@ import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composedropdown.ui.theme.ComposeDropDownTheme
 
@@ -29,10 +34,21 @@ class MainActivity : ComponentActivity() {
             ComposeDropDownTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color(0xFF101010),
+                    modifier = Modifier.fillMaxSize()
                 ) {
-
+                    DropDown(
+                        text = "Hello World!",
+                        modifier = Modifier.padding(15.dp)
+                    ) {
+                        Text(
+                            text = "This is now revealed!",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(100.dp)
+                                .background(Color.Green)
+                        )
+                    }
                 }
             }
         }
@@ -50,6 +66,12 @@ fun DropDown(
         mutableStateOf(initiallyOpened)
     }
     val alpha = animateFloatAsState(
+        targetValue = if (isOpen) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 300
+        )
+    )
+    val rotateX = animateFloatAsState(
         targetValue = if (isOpen) 0f else -90f,
         animationSpec = tween(
             durationMillis = 300
@@ -79,6 +101,19 @@ fun DropDown(
                     }
                     .scale(1f, if (isOpen) -1f else 1f)
             )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .graphicsLayer {
+                    transformOrigin = TransformOrigin(0.5f, 0f)
+                    rotationX = rotateX.value
+                }
+                .alpha(alpha.value)
+        ) {
+            content()
         }
     }
 }
