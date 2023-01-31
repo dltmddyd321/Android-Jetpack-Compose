42e0260003c730a93d0bfc1c85262541b4cb17b4
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2023-01-30 21:58:49 -0800

    LazyColumn
    - rememberCoroutineScope : 스크롤 갱신
    - rememberLazyListState : 스크롤 상태

--- ComposeUnderstand/app/src/main/java/com/example/composeunderstand/MainActivity.kt
+++ ComposeUnderstand/app/src/main/java/com/example/composeunderstand/MainActivity.kt
@@ -10,14 +10,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -34,6 +34,7 @@ import com.example.composeunderstand.ui.theme.ActionBtnBgColor
 import com.example.composeunderstand.ui.theme.ComposeUnderstandTheme
 import com.example.composeunderstand.ui.theme.Purple200
 import com.example.composeunderstand.ui.theme.Purple500
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
@@ -84,84 +85,127 @@ fun Calculator() {
     //현재 선택된 Symbol
     val selectedSymbol: String = selectedAction.value?.symbol ?: ""
 
-    LazyVerticalGrid(
+    val calHistories: MutableState<List<String>> = remember {
+        mutableStateOf(emptyList())
+    }
+
+    val coroutineScope = rememberCoroutineScope()
+    val scrollState = rememberLazyListState()
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        androidx.compose.material3.Card(
+            elevation = CardDefaults.cardElevation(8.dp),
+            onClick = {
+
+            }
+        ) {
+            Text(text = "계산 기록 보기")
+        }
+
+        LazyColumn(
+            state = scrollState,
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            contentPadding = PaddingValues(vertical = 10.dp),
+            reverseLayout = true, //위에서 부터 기록 추가
+            modifier = Modifier.weight(1f),
+            content = {
+                items(calHistories.value) { history ->
+                    Text(text = history, modifier = Modifier.background(Purple500))
+                }
+            })
+
+        LazyVerticalGrid(
 //        columns = GridCells.Adaptive(120.dp), // 120dp에 맞추어 채우겠다.
-        columns = GridCells.Fixed(4), //4칸으로 나누어 맞추겠다.
-        horizontalArrangement = Arrangement.spacedBy(8.dp), //가로 간격 조절
-        verticalArrangement = Arrangement.spacedBy(8.dp), //세로 간격 조절
-        content = {//넣을 아이템에 대해 명시
+            columns = GridCells.Fixed(4), //4칸으로 나누어 맞추겠다.
+            horizontalArrangement = Arrangement.spacedBy(8.dp), //가로 간격 조절
+            verticalArrangement = Arrangement.spacedBy(8.dp), //세로 간격 조절
+            content = {//넣을 아이템에 대해 명시
 
-            item(span = {
-                GridItemSpan(maxLineSpan)
-            }) {
-                NumberText(
-                    firstInput,
-                    secondInput,
-                    selectedSymbol,
-                    modifier = Modifier.fillMaxSize()
-                )
-            }
+                item(span = {
+                    GridItemSpan(maxLineSpan)
+                }) {
+                    NumberText(
+                        firstInput,
+                        secondInput,
+                        selectedSymbol,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
 
-            item(span = {
-                GridItemSpan(2)
-            }) {
-                ActionButton(action = CalAction.AllClear,
-                    onClicked = {
-                        firstInput = "0"
-                        secondInput = ""
-                        selectedAction.value = null
+                item(span = {
+                    GridItemSpan(2)
+                }) {
+                    ActionButton(action = CalAction.AllClear,
+                        onClicked = {
+                            firstInput = "0"
+                            secondInput = ""
+                            selectedAction.value = null
+                        })
+                }
+
+                item(span = {
+                    GridItemSpan(1)
+                }) {
+                    ActionButton(action = CalAction.Delete, onClicked = {
+
+                        if (secondInput.isNotEmpty()) {
+                            secondInput += secondInput.dropLast(1)
+                            return@ActionButton
+                        }
+
+                        if (selectedAction.value != null) {
+                            selectedAction.value = null
+                            return@ActionButton
+                        }
+                        //마지막 글자 1개 지우기
+                        firstInput = if (firstInput.length == 1) "0" else firstInput.dropLast(1)
                     })
-            }
+                }
 
-            item(span = {
-                GridItemSpan(1)
-            }) {
-                ActionButton(action = CalAction.Delete, onClicked = {
-
-                    if (secondInput.isNotEmpty()) {
-                        secondInput += secondInput.dropLast(1)
-                        return@ActionButton
-                    }
-
-                    if (selectedAction.value != null) {
-                        selectedAction.value = null
-                        return@ActionButton
-                    }
-                    //마지막 글자 1개 지우기
-                    firstInput = if (firstInput.length == 1) "0" else firstInput.dropLast(1)
-                })
-            }
-
-            //숫자와 계산 기호 처리
-            items(buttons) { btn ->
-                when (btn) {
-                    is CalAction -> ActionButton(btn, selectedAction.value, onClicked = {
-                        selectedAction.value = btn
-                    })
-                    is Int -> NumberButton(num = btn) {
-                        if (selectedAction.value == null) {
-                            if (firstInput == "0") firstInput =
-                                btn.toString() else firstInput += btn
-                        } else {
-                            secondInput += btn
+                //숫자와 계산 기호 처리
+                items(buttons) { btn ->
+                    when (btn) {
+                        is CalAction -> ActionButton(btn, selectedAction.value, onClicked = {
+                            selectedAction.value = btn
+                        })
+                        is Int -> NumberButton(num = btn) {
+                            if (selectedAction.value == null) {
+                                if (firstInput == "0") firstInput =
+                                    btn.toString() else firstInput += btn
+                            } else {
+                                secondInput += btn
+                            }
                         }
                     }
                 }
-            }
 
-            item(span = {
-                GridItemSpan(maxCurrentLineSpan) //여유 공간만큼 다 채운다.
-            }) {
-                ActionButton(action = CalAction.Calculate, onClicked = {
-                    val result = selectedAction.value?.let {
-                        startCalculate(firstInput.toFloat(), secondInput.toFloat(), it)
-                    } ?: Log.d("CAL", "선택된 연산이 없습니다.")
-                    firstInput = result.toString()
-                    secondInput = ""
-                    selectedAction.value = null
-                })
-            }
-        })
+                item(span = {
+                    GridItemSpan(maxCurrentLineSpan) //여유 공간만큼 다 채운다.
+                }) {
+                    ActionButton(action = CalAction.Calculate, onClicked = {
+                        val result = selectedAction.value?.let {
+                            startCalculate(firstInput.toFloat(), secondInput.toFloat(), it)
+                        } ?: Log.d("CAL", "선택된 연산이 없습니다.")
+
+                        //계산 기록 갱신
+                        val calHistory = "$firstInput $selectedSymbol $secondInput = $result"
+
+                        calHistories.value += calHistory
+
+                        coroutineScope.launch {
+                            scrollState.animateScrollToItem(calHistories.value.size) //마지막에 추가된. 즉, 맨 위로 스크롤
+                        }
+
+                        firstInput = result.toString()
+                        secondInput = ""
+                        selectedAction.value = null
+                    })
+                }
+            })
+    }
 }
 
 //계산 처리
