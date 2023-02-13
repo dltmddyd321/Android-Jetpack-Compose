f095af0a424dab191356b0a482367a6d26d967d8
이승용 <73208493+dltmddyd321@users.noreply.github.com>
2023-02-13 12:27:04 +0900

    컴포즈 이모지뷰

--- CheckInternet/app/src/main/java/com/example/checkinternet/MainActivity.kt
+++ CheckInternet/app/src/main/java/com/example/checkinternet/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.checkinternet
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.lifecycleScope
 import com.example.checkinternet.ui.theme.CheckInternetTheme
 import kotlinx.coroutines.flow.launchIn
@@ -36,13 +40,36 @@ class MainActivity : ComponentActivity() {
                 val status by connectivityObserver.observe().collectAsState(
                     initial = ConnectivityObserver.Status.Unavailable
                 )
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(text = "Network status: $status")
+                Row {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "Network status: $status")
+                    }
+                    EmojiView(emoji = "🥷")
                 }
             }
         }
+    }
+
+    @Composable
+    fun EmojiView(
+        emoji: String,
+    ) {
+        AndroidView(
+            factory = { context ->
+                AppCompatTextView(context).apply {
+                    text = emoji ?: "😟"
+                    textSize = 48.0F
+                    textAlignment = View.TEXT_ALIGNMENT_CENTER
+                }
+            },
+            update = {
+                it.apply {
+                    text = emoji ?: "😟"
+                }
+            },
+        )
     }
 }
